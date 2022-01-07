ae673d1dc4556934a9f679a3f1d48e46e9c8b06d
Roman Loyko <romabaz@gmail.com>
2022-01-07 23:15:46 +0200

    Initial working POC

---
+++ src/cellground.rs
@@ -0,0 +1,156 @@
+use std::vec::Vec;
+use sdl2::render::WindowCanvas;
+use sdl2::rect::{Rect};
+use sdl2::pixels::Color;
+
+pub struct Cellground {
+    cells: Vec<Vec<bool>>,
+    next_cells: Vec<Vec<bool>>,
+    rows: usize,
+    columns: usize,
+}
+
+impl Cellground {
+    pub fn new(rows: usize, columns: usize) -> Self {
+        let mut cells = Vec::with_capacity(rows);
+        let mut next_cells = Vec::with_capacity(rows);
+        for _ in 0..rows {
+            cells.push(vec![false; columns]);
+            next_cells.push(vec![false; columns]);
+        }
+        Self {
+            cells,
+            next_cells,
+            rows,
+            columns,
+        }
+    }
+
+    pub fn set_cell(&mut self, row: usize, column: usize) {
+        self.cells[row][column] = !self.cells[row][column];
+    }
+
+    pub fn clear_cells(&mut self) {
+        let mut cells = Vec::with_capacity(self.rows);
+            for _ in 0..self.rows {
+                cells.push(vec![false; self.columns]);
+            }
+        self.cells = cells;
+        self.next_cells = self.cells.clone();
+    }
+
+    pub fn next_gen(&mut self) {
+        for x in 0..self.columns {
+            for y in 0..self.rows {
+                if self.cells[y][x] {
+                    self.next_cells[y][x] = self.cells[y][x];
+                    if self.get_neighbours_cnt(x, y) < 2 || self.get_neighbours_cnt(x, y) > 3 {
+                        self.next_cells[y][x] = false;
+                    }
+                } else {
+                    if self.get_neighbours_cnt(x, y) == 3 {
+                        self.next_cells[y][x] = true;
+                    }
+                }
+            }
+        }
+        self.cells = self.next_cells.clone();
+    }
+
+    fn get_neighbours_cnt(&self, x: usize, y: usize) -> i32 {
+        let mut cnt :i32 = 0;
+        if x == 0 && y == 0 {
+            cnt += self.cells[1][1] as i32;
+            cnt += self.cells[0][1] as i32;
+            cnt += self.cells[1][0] as i32;
+            return cnt;
+        }
+
+        if x == self.columns - 1 && y == self.rows - 1 {
+            cnt += self.cells[self.rows - 2][self.columns - 2] as i32;
+            cnt += self.cells[self.rows - 1][self.columns - 2] as i32;
+            cnt += self.cells[self.rows - 2][self.columns - 1] as i32;
+            return cnt;
+        }
+
+        if x == self.columns - 1 && y == 0 {
+            cnt += self.cells[0][x - 1] as i32;
+            cnt += self.cells[1][x - 1] as i32;
+            cnt += self.cells[1][x] as i32;
+            return cnt;
+        }
+
+        if x == 0 && y == self.rows - 1 {
+            cnt += self.cells[y - 1][0] as i32;
+            cnt += self.cells[y - 1][1] as i32;
+            cnt += self.cells[y][1] as i32;
+            return cnt;
+        }
+
+        if x == 0 && (y < self.rows - 1 || y > 0) {
+            cnt += self.cells[y][1] as i32;
+            cnt += self.cells[y - 1][1] as i32;
+            cnt += self.cells[y + 1][1] as i32;
+            cnt += self.cells[y - 1][0] as i32;
+            cnt += self.cells[y + 1][0] as i32;
+            return cnt;
+        }
+
+        if y == 0 && (x < self.columns - 1 || x > 0) {
+            cnt += self.cells[1][x] as i32;
+            cnt += self.cells[1][x - 1] as i32;
+            cnt += self.cells[1][x + 1] as i32;
+            cnt += self.cells[0][x - 1] as i32;
+            cnt += self.cells[0][x + 1] as i32;
+            return cnt;
+        }
+
+        if x == self.columns - 1 && (y < self.rows - 1 || y > 0) {
+            cnt += self.cells[y][self.columns - 2] as i32;
+            cnt += self.cells[y - 1][self.columns - 2] as i32;
+            cnt += self.cells[y + 1][self.columns - 2] as i32;
+            cnt += self.cells[y - 1][self.columns - 1] as i32;
+            cnt += self.cells[y + 1][self.columns - 1] as i32;
+            return cnt;
+        }
+
+        if y == self.rows - 1 && (x < self.columns - 1 || x > 0) {
+            cnt += self.cells[self.rows - 2][x] as i32;
+            cnt += self.cells[self.rows - 2][x - 1] as i32;
+            cnt += self.cells[self.rows - 2][x + 1] as i32;
+            cnt += self.cells[self.rows - 1][x - 1] as i32;
+            cnt += self.cells[self.rows - 1][x + 1] as i32;
+            return cnt;
+        }
+
+        cnt += self.cells[y][x - 1] as i32;
+        cnt += self.cells[y - 1][x - 1] as i32;
+        cnt += self.cells[y - 1][x] as i32;
+        cnt += self.cells[y - 1][x + 1] as i32;
+        cnt += self.cells[y][x + 1] as i32;
+        cnt += self.cells[y + 1][x + 1] as i32;
+        cnt += self.cells[y + 1][x] as i32;
+        cnt += self.cells[y + 1][x - 1] as i32;
+        cnt
+    }
+}
+
+pub trait Renderable {
+    fn render_thing(&self, _: &mut WindowCanvas);
+}
+
+impl Renderable for Cellground {
+    fn render_thing(&self, canvas: &mut WindowCanvas) {
+        for x in 0..self.columns {
+            for y in 0..self.rows {
+                if self.cells[y][x] {
+                    canvas.set_draw_color(Color::RGB(0, 0, 0));
+                    canvas.fill_rect(Rect::new((x as i32) * 10 + 1, (y as i32) * 10 + 1, 9, 9)).unwrap();
+                } else {
+                    canvas.set_draw_color(Color::RGB(255, 255, 255));
+                    canvas.fill_rect(Rect::new((x as i32) * 10 + 1, (y as i32) * 10 + 1, 9, 9)).unwrap();
+                }
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,75 @@
+mod cellground;
+
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::mouse::MouseButton;
+use std::time::Duration;
+use sdl2::rect::{Point};
+use cellground::{Cellground, Renderable};
+
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    const CANVAS_WIDTH: i32 = 800;
+    const CANVAS_HEIGHT: i32 = 600;
+    let window = video_subsystem.window("Convey's Game of Life", CANVAS_WIDTH as u32, CANVAS_HEIGHT as u32)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    canvas.clear();
+    canvas.set_draw_color(Color::RGB(0, 100, 255));
+    let mut xl = 0;
+    let mut yl = 0;
+    loop {
+        canvas.draw_line(Point::new(xl, 0), Point::new(xl, CANVAS_HEIGHT)).unwrap();
+        canvas.draw_line(Point::new(0, yl), Point::new(CANVAS_WIDTH, yl)).unwrap();
+        if yl < CANVAS_HEIGHT as i32 {
+            xl += 10;
+            yl += 10;
+            continue;
+        }
+        if xl < CANVAS_WIDTH {
+            xl += 10;
+            continue;
+        }
+        break;
+    }
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut cellground = Cellground::new(CANVAS_HEIGHT as usize / 10, CANVAS_WIDTH as usize / 10);
+    'running: loop {
+        // i = (i + 1) % 255;
+        // canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        // canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                Event::MouseButtonDown { mouse_btn: MouseButton::Left, x, y, .. } => {
+                    cellground.set_cell(y as usize / 10, x as usize / 10);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Space), .. } => {
+                     cellground.clear_cells();
+                },
+                Event::KeyDown { keycode: Some(Keycode::N), .. } => {
+                    cellground.next_gen();
+                },
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+        cellground.render_thing(&mut canvas);
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
